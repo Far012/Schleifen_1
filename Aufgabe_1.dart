a007dff2372cf810beb31f68d03594aec5294fa6
Farhad <farhad.ahmadi@outlook.de>
2025-08-22 01:54:22 -0700

    Final

--- Aufgabe_1.dart
+++ Aufgabe_1.dart
@@ -3,10 +3,12 @@ void main() {
 List<int> points = [4, 5, 4, 2, 6, 6, 3];
 List<String> names = ['Julietta', 'Benjamino', 'Hans-Günther', 'Evalinea', 'Fiona', 'Gregory', 'Leonhart'];
 
+//Aufgabe 1
 for (String name in names) {
   print(name);
 }
 
+//Aufgabe 2
 int sum = 0;
 for (int point in points) {
   sum += point;
